2a5440dbe6978e63c5cf2ab6f8cd5e0ab5d40866
li xin <2237498407@qq.com>
2021-08-31 10:27:50 +0800

    提交测试

--- src/main/java/com/lixnstudy/webcrawler/testCode/MyTest.java
+++ src/main/java/com/lixnstudy/webcrawler/testCode/MyTest.java
@@ -20,13 +20,15 @@ import org.jsoup.select.Elements;
  * @create 2021/8/30 3:08 下午
  **/
 public class MyTest {
+    // TODO 保持登陆所需的Cookie
+    private static final String cookieValue = "ll=\"118281\"; bid=xXOFVbD9elY; dbcl2=\"245370804:7ULN5r+hQKc\"; ck=M8Af";
+
     public static void main(String[] args) throws Exception{
-        String url = "https://movie.douban.com/top250";
-        String anotherUrl = "https://movie.douban.com/subject/1291546/";
-        String cookieValue = "ll=\"118281\"; bid=xXOFVbD9elY; dbcl2=\"245370804:7ULN5r+hQKc\"; ck=M8Af";
+        String top250Url = "https://movie.douban.com/top250";
+        String detailsUrl = "https://movie.douban.com/subject/1291546/";
         HttpClient client = HttpClientBuilder.create()
                 .build();
-        HttpGet get = new HttpGet(anotherUrl);
+        HttpGet get = new HttpGet(detailsUrl);
         get.setHeader("Cookie",cookieValue);
         get.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
         HttpResponse response = client.execute(get);
@@ -43,28 +45,27 @@ public class MyTest {
 //            }
 //            String picLink = pic.attr("href");
 //            System.out.println(picLink);
-//            HttpGet httpGet = new HttpGet(picLink);
-//            System.out.println(count + " time ready to execute get request" );
-//            httpGet.setHeader("Cookie",cookieValue);
-//            HttpResponse response1 = client.execute(httpGet);
-//            httpGet.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
-//            System.out.println(count + ": result is " + response1.getStatusLine().getStatusCode());
-////            String htmlCode = EntityUtils.toString(client.execute(httpGet).getEntity(), "UTF-8");
-////            Document doc = Jsoup.parse(htmlCode);
-////            Elements elements = doc.select("span.pl");
-////            for (Element element : elements) {
-////                String director = element.text();
-////                System.out.println("director is " + director);
-////            }
-//            count++;
 //        }
 
 
         System.out.println(response.getStatusLine().getStatusCode());
 //        System.out.println(response.getStatusLine().getStatusCode());
-//        System.out.println(EntityUtils.toString(response.getEntity(), "UTF-8"));
+//        System.out.println(EntityUtils.toString(response.getEntity(), "UTF-8")); 这一句会导致流没有关闭，意思就是这个方法是通过流来处理Entity的
 
+    }
+
+    /**
+     * 创建Get请求进入详情页
+     */
+    private static HttpGet generateGetRequest(String url) {
+        HttpGet get = new HttpGet(url);
+        get.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36");
+        get.setHeader("Cookie", "");
+        return get;
     }
 
 
 }
+
+
+
